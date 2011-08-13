4341176687be6d708cc56aa53c8b985cf0c24f1d
Joseph Vaughan <joe.vaughan92@gmail.com>
2011-08-13 21:30:33 +0100

    Logic fixes, CPU usage fixes (I hope), trying to catch socket exceptions

--- daemon.py
+++ daemon.py
@@ -31,7 +31,7 @@ class Daemon(object):
 			sys.exit(1)
 	
 		# decouple from parent environment
-		#os.chdir("/") 
+		os.chdir("/") 
 		os.setsid() 
 		os.umask(0) 
 	
@@ -44,16 +44,6 @@ class Daemon(object):
 		except OSError, e: 
 			sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
 			sys.exit(1) 
-	
-		# redirect standard file descriptors
-		#sys.stdout.flush()
-		#sys.stderr.flush()
-		#si = file(self.stdin, 'r')
-		#so = file(self.stdout, 'a+')
-		#se = file(self.stderr, 'a+', 0)
-		#os.dup2(si.fileno(), sys.stdin.fileno())
-		#os.dup2(so.fileno(), sys.stdout.fileno())
-		#os.dup2(se.fileno(), sys.stderr.fileno())
 	
 		# write pidfile
 		atexit.register(self.delpid)
